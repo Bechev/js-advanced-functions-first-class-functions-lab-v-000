b55237928922e2e1bf169a032d41eae571491c5e
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-08-15 11:53:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers = function (list){
   return list.slice(0,2);
-}
+};
 
 
 const returnLastTwoDrivers = function(list){
   retrun list.slice(-2);
-}
+};
